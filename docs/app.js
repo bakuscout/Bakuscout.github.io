2f6b8198e41367bfa270e8abed81cff033c14097
Isaac Anderson <redranger.anderson@gmail.com>
2025-03-12 19:55:57 -0500

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,2 +1,28 @@
 console.log("Site Loaded");
-alert("Do not login!");
+alert("Feel free to login!");
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const now = new Date();
+    document.cookie = `now = ${now}; SameSite=None: Secure`;
+    document.cookie = "course = WebProgramming2025; SameSite=None; Secure";
+
+    document.getElementById("old-cookies").innerText = document.cookie;
+
+    const cookieStore = window.cookieStore;
+
+    cookieStore.set({
+        name: "username",
+        value: "Eleana Thraseer"
+    }).then(() => {
+        console.log("Cookie set using cookieStore");
+    }, (reason) => {
+        console.log("Unable to set cookie: " + reason);
+    });
+    cookieStore.get("username").then(
+        (obj) => {
+            const elt = document.getElementById("new-cookies");
+            Element.innerText = `${obj.name} = ${obj.value}}`;
+        },
+        (reason) => {"Unable to read cookie: " + reason}
+    );
+});
